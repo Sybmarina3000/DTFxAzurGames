b9cd0b6eef5e35746b7aa908a1ca3308e9722618
MarinaPorkhunova <MarinaPorkhunova@MyTona.com>
2020-07-19 02:33:08 +0300

    Added first game animation;

---
+++ DTF2020/Assets/Scripts/FirstCameraGameAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FirstCameraGameAnimation : MonoBehaviour
+    {
+        [SerializeField] private GameObject _firstPoint;
+        [SerializeField] private GameObject _finishPoint;
+        [Space(10)]
+        [SerializeField] private float _firstPauseTime;
+        [Header(" speed move == first->finish + pause ")]
+        [SerializeField] private float _finishPauseTime;
+        [Header(" speed move == finish->first + pause before player spawn ")]
+        [SerializeField] private float _returnTime;
+
+        private playerController _player;
+        private void Start()
+        {
+            _player = RealizeBox.instance.player;
+            _firstPoint.SetActive(true);
+            StartCoroutine("Animation");
+        }
+
+        private IEnumerator Animation()
+        {
+            yield return new WaitForSeconds(_firstPauseTime);
+            _finishPoint.SetActive(true);
+            
+            yield return new WaitForSeconds(2 + _firstPauseTime);
+            _finishPoint.SetActive(false);
+            
+            yield return new WaitForSeconds(_returnTime);
+            _player.gameObject.SetActive(true);
+            yield break;
+        }
+    }
+}
