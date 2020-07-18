55e1c22f39c956883b961b5b6f44fba3e3965b58
MarinaPorkhunova <MarinaPorkhunova@MyTona.com>
2020-07-18 23:31:56 +0300

    Added normal ui direction ( from, to points) ;

--- DTF2020/Assets/Scripts/Conus/DirectionConus.cs
+++ DTF2020/Assets/Scripts/Conus/DirectionConus.cs
@@ -19,20 +19,47 @@ namespace DefaultNamespace
 
         [SerializeField] private Vector3 _currentVector;
 
-        private Image _conusImage; 
+        private Image _conusImage;
+
+        private RectTransform _conusRectTransform;
+        
+        [SerializeField] private Transform _from;
+        
+        [SerializeField] private Transform _to;
+        
         
         private void Awake()
         {
             _conusImage = GetComponent<Image>();
+            _conusRectTransform = GetComponent<RectTransform>();
+            
             _conusImage.fillAmount = ((float)_radius) / 360;
-            StartCoroutine(StartArrowLocation());
         }
 
+        public void TestSpawn()
+        {
+            Spawn(_from.position, _to.position);
+        } 
+        
         void Spawn(Vector3 from, Vector3 to)
         {
+
+            _conusRectTransform.position = Camera.main.WorldToScreenPoint(from);
+
+            var vectorDirection = to - from;
+            var angle = Vector3.Angle(Vector3.up, vectorDirection) - (0.5f * _radius);
+            
+            _conusRectTransform.rotation = Quaternion.identity;
+            _conusRectTransform.Rotate(Vector3.back, angle);
+            
             StartCoroutine(StartArrowLocation());
         }
 
+        void Apply()
+        {
+            
+        }
+        
         IEnumerator StartArrowLocation()
         {
             int direction = 1;
@@ -42,12 +69,20 @@ namespace DefaultNamespace
             
             while (true)
             {
-                var angle = (_radius / _timeOneRound) * Time.deltaTime * direction;
+                float gran = 0;
+                if (currentAngle > _radius || currentAngle < 0)
+                {
+                    direction = direction * (-1);
+                    if (currentAngle > _radius)
+                        gran = _radius - currentAngle;
+                    else
+                        gran = currentAngle * (-1);
+                }
+                
+                var angle = gran + (_radius / _timeOneRound) * Time.deltaTime * direction;
                 _arrow.Rotate( Vector3.back, angle);
                 
                 currentAngle += angle;
-                if (currentAngle > _radius || currentAngle < 0 )
-                    direction = direction * (-1);
                 
                 yield return null;
             }
