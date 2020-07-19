4148a242720afc62a1d06208e3e72e8c6f553a09
MarinaPorkhunova <MarinaPorkhunova@MyTona.com>
2020-07-19 14:29:28 +0300

    Added lazer;

---
+++ DTF2020/Assets/Scripts/GameLogic/Lazer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Lazer : MonoBehaviour
+    {
+        private Score _score;
+        
+        [SerializeField] private int _damage;
+        
+        private void Start()
+        {
+            _score = RealizeBox.instance.score;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                Debug.Log("WOW");
+                SetDamage();
+            }
+        }
+
+        private void SetDamage()
+        {
+            _score.Decrease(_damage, ScoreDecreaseType.Damage);
+        }
+    }
+}
