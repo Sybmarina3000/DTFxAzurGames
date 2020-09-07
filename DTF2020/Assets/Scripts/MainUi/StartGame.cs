7ac859a160c45da9aa648c5bcca335e086d8d893
MarinaPorkhunova <MarinaPorkhunova@MyTona.com>
2020-09-07 22:56:31 +0300

    Add;

---
+++ DTF2020/Assets/Scripts/MainUi/StartGame.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public int currentLvl;
+
+    [SerializeField] private TextMeshProUGUI text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("currentLvl") == false)
+        {
+            currentLvl = 0;
+        }
+        else
+        {
+            currentLvl = PlayerPrefs.GetInt("currentLvl");
+        }
+
+        text.text = (currentLvl + 1).ToString() + " level";
+    }
+
+    // Update is called once per frame
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("level" + ((currentLvl % 4) + 1)); 
+    }
+}
